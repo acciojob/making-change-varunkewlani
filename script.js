09dd41d24c402b79e45a53c655f9d2be883f5e98
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-26 00:39:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const makeChange = (c) => {
+	let user ={
+		q: 25,
+		d: 10,
+		n: 5,
+		p, 1
+	}
+	let arr=[]
+	for(let i=0;i<4;i++)
+		{
+			let count=0;
+			while(user.q>=c)
+			{
+				count++
+				c-=user.q
+			}
+			arr.push(count)
+		}
+	
+		return("{ "q":",arr[0],"""d":"",arr[1],""n":",arr[2],"""p":"",arr[3],"}")
+	
   // your name here
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+const c = /prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
