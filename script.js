bc33b75b5efdcded137ee513ce425e7d02588b86
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-26 03:41:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const makeChange = (c) => {
 	let user ={
-		0: [q, 25],
-		1: [d, 10],
-		2: [n, 5],
-		3: [p, 1]
+		0: ['q', 25],
+		1: ['d', 10],
+		2: ['n', 5],
+		3: ['p', 1]
 	}
 	let arr=[]
 	for(let i=0;i<4;i++)
@@ -12,7 +12,7 @@ const makeChange = (c) => {
 			while(user[i][1]<=c)
 			{
 				count++
-				c-=user.[i][1]
+				c-=user[i][1]
 			}
 			arr.push(count)
 		}
@@ -20,7 +20,7 @@ const makeChange = (c) => {
 		return { "q": arr[0], "d": arr[1], "n": arr[2], "p": arr[3] }
 	
   // your name here
-};
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ")
